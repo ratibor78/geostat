cd83de669629df582254c206cbf86ecc99543ca7
Alexey <alexey@polarbear.partners>
2018-10-10 10:30:56 +0300

    change some things in install.sh

--- install.sh
+++ install.sh
@@ -1,26 +1,32 @@
 #! /usr/bin/env bash
 
 ##
-## Installetion for GeoStat script
+## Installetion script for GeoStat
+## Alexey Nizhegolenko 2018
 ##
 
 echo "Creating virtual ENV and installing requirements..."
-
 virtualenv venv && source venv/bin/activate
+sleep 1
 
-pip install -r requirements.txt
+pip install -r requirements.txt && deactivate
+sleep 1
 
+echo "Please edit settings.ini file and set right parameters..."
 cp settings.ini.back settings.ini
 
 "${VISUAL:-"${EDITOR:-vi}"}" "settings.ini"
 
+
+echo "Installing SystemD service..."
 while read line
 do
     eval echo "$line"
 done < "./geostat.service.template" > /lib/systemd/system/geostat.service
 
 systemctl enable geostat.service
+sleep 1
 
 echo "All done, now you can start getting GEO data from your log"
-echo "run 'systemd start geostat.service' "
-echo "Good Luck"
+echo "run 'systemd start geostat.service' for this"
+echo "Good Luck !"
